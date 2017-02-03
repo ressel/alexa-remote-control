f4571549e1ab1d86a97477865fbe1d6bb0370537
ressel <ressel@users.noreply.github.com>
2017-02-03 21:03:33 +0100

    Add variable for AWSHOST

--- alexa-pause.sh
+++ alexa-pause.sh
@@ -4,7 +4,7 @@
 . ./alexa-device-echo.sh
 
 CMD='{"type":"PauseCommand"}'
-URL='https://layla.amazon.de/api/np/command?deviceSerialNumber='$DEVICENUMBER'&deviceType='$DEVICETYPE
+URL='https://'AWSHOST'/api/np/command?deviceSerialNumber='$DEVICENUMBER'&deviceType='$DEVICETYPE
 echo curl $URL -H "$COOKIE" -H "$CSRF" --data "${CMD}"
 curl $URL \
 -H "$COOKIE" \
